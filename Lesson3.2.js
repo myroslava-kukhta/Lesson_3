b505af4a31ee667539329520271f15a75525f99a
myroslava-kukhta <miroslavakukhta@gmail.com>
2023-02-17 13:53:25 +0200

    Lesson_3.1

---
+++ Lesson3.2.js
@@ -0,0 +1,4 @@
+  let n = 12345;
+  let newn = n.toString;
+  newn.split([' ', 5]);
+  console.log(newn);
---
+++ Lesson_3.01.js
@@ -0,0 +1,4 @@
+let string = prompt ('write something', 'a');
+let string2 = prompt ('write something', 'b');
+let string3 = prompt ('write something', 'c');
+alert(string.concat('', string2, string3));
